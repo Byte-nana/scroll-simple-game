e11997c2ebd5a67aeb8df54339ac4823289ec439
Byte-nana <nanadevelop44@gmail.com>
2025-10-08 22:42:52 +0900

    feat:add going up to the top page button

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 'use strict';
 
-const btn = document.querySelector('.find-btn');
+const findBtn = document.querySelector('.find-btn');
 const rabbit = document.querySelector('.rabbit');
+const arrowUp = document.querySelector('.arrow-up');
 
-btn.addEventListener('click', () => {
+findBtn.addEventListener('click', () => {
   rabbit.scrollIntoView({ block: 'center', behavior: 'smooth' });
+});
+
+arrowUp.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'smooth',
+  });
 });
